4a51ae4c472b931db158ed9026351c3e4c40f37a
Anastasia <anastasiastarodub@gmail.com>
2023-03-11 23:42:32 +0300

    решение задачи 4

---
+++ DZ4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая на вход принимаеи число (N), 
+// а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ for (int count = 1; count <= N; count++)
+ {
+    if (count % 2 == 0)
+    {
+        Console.WriteLine($"Четное число - {count}");
+    }
+ }
+
